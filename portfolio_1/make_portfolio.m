8bc3dd00e6d9e594ae74d7741a397bf44b66d68c
Jesse <mr.jesselu@gmail.com>
2013-08-12 15:00:00 -0700

    Simple make portfolio works.

--- portfolio_1/make_portfolio.m
+++ portfolio_1/make_portfolio.m
@@ -3,10 +3,11 @@ function [x] = make_portfolio(mu)
     [c, ~, A, B, C] = sim_matrices();
     N = length(c); % N = m * n;
 
+    cvx_precision low
     cvx_begin
         variable x(N)
         minimize -(c'*x) + mu * std(A*x)
         subject to 
-            norm(x) <= 1
-            norm(B*x) <= 0.2
+            norm(x, 1) <= 1
+            norm(B*x, 1) <= 0.2
     cvx_end
